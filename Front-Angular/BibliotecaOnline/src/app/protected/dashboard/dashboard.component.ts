ecff35c6d773eaabcbbac58718eed75303395711
rebofel <feliperebolledo@outlook.com>
2022-10-03 00:11:32 -0300

    Endpoint trae listado de prestamos a dashboard usuario. Falta mejorar

--- Front-Angular/BibliotecaOnline/src/app/protected/dashboard/dashboard.component.ts
+++ Front-Angular/BibliotecaOnline/src/app/protected/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,38 +14,43 @@ export class DashboardComponent implements OnInit{
 rol: string = localStorage.getItem('role') || '';
 
 
-nothing = [] = []; 
-
+lendings : any[]= [];
+  ProtectedService: any;
+  private baseUrl: string = environment.baseUrl;
   get usuario() {
 
     return this.authService.user; 
   }
   constructor( private router: Router,
-               private authService: AuthService) { }
+               private authService: AuthService,
+               private http: HttpClient) { }
+                           //No me toma la interface asi que por ahora que devuelva any, a la bosta
+               traerLendings(): Observable<any> {
+
+                const url = `${this.baseUrl}/lendings/`;
+                const headers = new HttpHeaders()
+                .set('Authorization',localStorage.getItem('token') || ''); // o String vacio. 
+                return this.http.get<any>(url, { headers }) 
+            
+              }
 
   ngOnInit(): void {
+    this.traerlosLendings();
    
-
-
-    if( window.localStorage )
-    {
-      if( !localStorage.getItem('firstLoad') )
-      {
-        localStorage['firstLoad'] = true;
-        window.location.reload();
-      }  
-      else
-        localStorage.removeItem('firstLoad');
-    }
+   
+    
+    console.log('los lendings')
+  
   }
                
 
-logout() {
+traerlosLendings() {
 
-this.authService.logout();
-this.router.navigateByUrl('/auth')
+this.traerLendings().subscribe (resp => {
+  console.log(resp.content);
+  this.lendings = resp.content;
 
+
+})
 }
-
-
 }
